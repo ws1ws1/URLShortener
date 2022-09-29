75fd639e1292c8ff67083db275e2473fe34e1b92
Andrew av <ws1rq7@gmail.com>
2022-09-29 18:59:27 +0300

    2909

--- URLShortener/wwwroot/js/JavaScript.js
+++
@@ -1,7 +0,0 @@
-﻿let listLink = document.querySelectorAll('.shortLink')
-for (let item of listLink) {
-    item.onclick = function (e) {
-        let t = e.target.getAttribute('longUrl')
-        window.open(t);
-    }
-}
